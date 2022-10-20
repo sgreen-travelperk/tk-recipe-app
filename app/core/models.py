979777e7ddc23f4e0a94dd6fdcfd197a90c235eb
sgreen-travelperk <scott.green@travelperk.com>
2022-10-20 17:25:59 +0200

    add ingredient model

--- app/core/models.py
+++ app/core/models.py
@@ -11,3 +11,16 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Ingredient(models.Model):
+    """Recipe object"""
+    name = models.CharField(max_length=255)
+    recipe = models.ForeignKey(
+        Recipe,
+        related_name='ingredients',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,7 +13,19 @@ class ModelTests(TestCase):
         """Test creating a recipe is successful"""
         recipe = models.Recipe.objects.create(
             name='Bacon Butty',
-            description='A perfect meal for a lazy sunday'
+            description='A perfect meal for a lazy sunday',
         )
 
         self.assertEqual(str(recipe), recipe.name)
+
+    def test_create_ingredient(self):
+        """Test creating a recipe is successful"""
+        recipe = models.Recipe.objects.create(
+            name='Bacon Butty',
+            description='A perfect meal for a lazy sunday',
+        )
+        ingredient = models.Ingredient.objects.create(
+            name='Bacon',
+            recipe=recipe
+        )
+        self.assertEqual(str(ingredient), ingredient.name)
